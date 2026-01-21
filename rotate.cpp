f2114d8ec35319d9c2203521fbff91d3daa6529e
Pratyaksh <reach.pratyaksh@gmail.com>
2026-01-21 19:29:35 +0530

    enhance the program to have the option to right shift too

--- rotate.cpp
+++ rotate.cpp
@@ -1,5 +1,3 @@
-//Left rotate an array by k positions
-
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -8,6 +6,7 @@ int main()
 {
     int size;
     int k;
+    int program;
     cout << "Enter the size of the Vector: ";
     cin >> size;
     
@@ -18,29 +17,65 @@ int main()
         cin >> nums[i];
     }
 
-    cout << "Enter the number of positions you want to shift the vector by: ";
-    cin >> k;
+    cout << "\nChoose the Program that you want to use: " << endl;
+    cout << "Enter 0 for Left Shift" << endl;
+    cout << "Enter 1 for Right Shift" << endl;
+    cin >> program;
 
-    int temp1 = 0;
-    int last = size-1;
-
-    cout << "\nInitial Vector: " << endl;
-    for (int i=0;i<size;i++)
+    if(program==0)
     {
-        cout << nums[i] << ",";
-    }
 
-    for (int i=0;i<k;i++)
-    {
-        temp1 = nums[0];
-        
-        for(int j=0;j<size;j++)
+        cout << "\nEnter the number of positions you want to shift the vector by: ";
+        cin >> k;
+
+        int temp1 = 0;
+        int last = size-1;
+
+        cout << "\nInitial Vector: " << endl;
+        for (int i=0;i<size;i++)
         {
-            nums[j] = nums[j+1];
-            
+            cout << nums[i] << ",";
         }
-        
-        nums[last] = temp1;
+
+        for (int i=0;i<k;i++)
+        {
+            temp1 = nums[0];
+            
+            for(int j=0;j<size;j++)
+            {
+                nums[j] = nums[j+1];
+                
+            }
+            
+            nums[last] = temp1;
+        }
+    }
+    else
+    {
+        cout << "\nEnter the number of positions you want to shift the vector by: ";
+        cin >> k;
+
+        int temp2 = 0;
+        int last = size-1;
+
+        cout << "\nInitial Vector: " << endl;
+        for (int i=0;i<size;i++)
+        {
+            cout << nums[i] << ",";
+        }
+
+        for (int i=0;i<k;i++)
+        {
+            temp2 = nums[last];
+            
+            for(int j=size-1;j>=0;j--)
+            {
+                nums[j] = nums[j-1];
+                
+            }
+            
+            nums[0] = temp2;
+        }
     }
 
     cout << "\nFinal Vector: " << endl;
@@ -49,5 +84,8 @@ int main()
         cout << nums[i] << ",";
     }
 
+    cout << "\n\nBYE~" << endl;
+
     return 0;
+
 }
