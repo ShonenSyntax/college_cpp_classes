c0a978ae3e725148a63745de0a8148da3e372da9
Pratyaksh <reach.pratyaksh@gmail.com>
2026-03-20 00:07:39 +0530

    implement linked list and it's operations

---
+++ linked_list.cpp
@@ -0,0 +1,113 @@
+#include <iostream>
+using namespace std;
+
+struct node{
+    int data;
+    node* next;
+};
+
+struct node* create_node(int x){
+    node* n = new node;
+    n -> data = x;
+    n -> next = NULL;
+
+    return n;
+}
+
+void traverse(node* head){
+    node* t = head;
+
+    while(t != NULL){
+        cout << t->data << endl;
+        t = t->next;
+    }
+}
+
+void search(node* head,int target){
+
+    node* t = head;
+    while(t){
+        if(t->data == target){
+            cout << "\nElement Found";
+            return;
+        }
+        t = t->next;
+    }
+    cout << "\nNot Found";
+        
+}
+
+node* insert_at_beginning(node* head,int value){
+    node* n0 = new node;
+    n0 -> data = value;
+    n0 -> next = head;
+
+    head = n0;
+
+    return head;
+}
+
+node* insert_end(node* head,int value){
+    node* n8 = new node;
+    n8-> data = value;
+    n8-> next = NULL;
+
+    if(head == NULL){
+        return n8;
+    }
+
+    node* t = head;
+
+    while(t->next!=NULL){
+        t = t -> next;
+    }
+    t->next = n8;
+
+    return head;
+}
+
+node* delete_begin(node* head){
+    if(head == NULL)
+        return NULL;
+
+    node* temp = head;
+    head = head->next;
+    delete temp;
+    
+    return head;
+}
+
+node* delete_end(node* head){
+    if(head == NULL)
+        return NULL;
+    
+    if(head->next == NULL){
+    delete head;
+    return NULL;
+    }
+
+    node* t = head;
+    while(t->next->next != NULL){
+        t = t->next;
+    }
+    delete t->next;
+    t->next = NULL;
+
+    return head;
+}
+
+int main(){
+    node* head = create_node(10);
+    node* n2 = create_node(30);
+    node* n3 = create_node(50);
+    head -> next = n2;
+    n2 -> next = n3;
+
+    head = delete_end(head);
+    
+    traverse(head);
+
+    return 0;
+}
+
+
