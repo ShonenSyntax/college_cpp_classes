183a55f2af89c4dad10a6789719c9aeb5bbdffcf
Pratyaksh <reach.pratyaksh@gmail.com>
2026-03-27 00:50:37 +0530

    implement stack using linked list

--- stack.cpp
+++ stack.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 #define MAX 4
 
-class stack{
+/* class stack{
     int arr[MAX];
     int top;
 
@@ -42,11 +42,67 @@ class stack{
             }
             cout << "\n" << arr[top] << endl;
         }
+}; */
+
+struct node{
+    int data;
+    node* next;
 };
 
+node* top = NULL;
+
+// Push (Insert at beginning)
+void push(int x){
+    node* n = new node;
+    n->data = x;
+    n->next = top;
+    top = n;
+
+    cout << x << " inserted\n";
+}
+
+// Pop (Delete from beginning)
+void pop(){
+    if(top==NULL){
+        cout << "Underflow\n" << endl;
+        return;
+    }
+    node* temp = top;
+    top = top->next;
+    cout << temp->data << " deleted\n";
+    delete temp;
+}
+
+// Peek (View the top Element)
+void peek(){
+    if(top==NULL){
+        cout << "Stack is empty\n";
+        return;
+    }
+    cout << "Top Element: " << top->data << endl;
+}
+
+// Display (View all the elements of the Stack)
+void display(){
+    if(top==NULL){
+        cout << "Stack is empty\n";
+        return;
+    }
+    node* t = top;
+    cout << "\nStack: " << endl;
+    while(t!=NULL){
+        cout << t->data << endl;
+        t = t->next;
+    }
+    cout << endl;
+}
+
+
+
 int main(){
-    stack a;
-    a.peek();
-    a.push(3);
-    a.peek();
+    push(10);
+    push(20);
+    display();
+    pop();
+    display();
 }
